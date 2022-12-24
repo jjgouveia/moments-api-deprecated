a86264fe6f39f410b2286dd3e60d20a57c82cb70
Jr Gouveia <gouvik.dev@gmail.com.br>
2022-12-24 17:48:58 -0300

    criado a implementação do verbo GET da rota Moments

---
+++ app/Controllers/Http/MomentsController.ts
@@ -0,0 +1,49 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Application from '@ioc:Adonis/Core/Application'
+import { v4 as uuidv4 } from 'uuid'
+
+import Moment from 'App/Models/Moment'
+
+export default class MomentsController {
+  private validationOptions = {
+    types: ['image'],
+    size: '2mb',
+  }
+
+  // método de criar um Moment
+  public async store({ request, response }: HttpContextContract) {
+    const body = request.body()
+
+    const image = request.file('image', this.validationOptions)
+
+    if (image) {
+      //cria um namefile único garantindo que dados não sejam sobrescritos no banco
+      const imageName = `${uuidv4()}.${image.extname}`
+      //move as imagens para a past tmp/uploads do sistema e seta o nome escolhido
+      await image.move(Application.tmpPath('uploads'), {
+        name: imageName,
+      })
+
+      //adiciona o mesmo nome ao body
+      body.image = imageName
+    }
+
+    //cria o Moment a partir dos dados fornecidos pelo body
+    const moment = await Moment.create(body)
+
+    response.status(201)
+
+    return {
+      message: 'Momento Criado com Sucesso',
+      data: moment,
+    }
+  }
+
+  public async index() {
+    const moments = await Moment.all()
+
+    return {
+      data: moments,
+    }
+  }
+}
